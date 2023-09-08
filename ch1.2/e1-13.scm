e5f126b71721d283b66ea0512d0901b08afa6094
Sam Johnson <sambjohnson94@gmail.com>
2023-09-08 00:18:32 -0700

    add naive ch1.2 exercises

---
+++ ch1.2/e1-13.scm
@@ -0,0 +1,4 @@
+;; Exercise 1.13: Prove that Fib(n) is the closest integer to φ n / 5, where
+;; φ = (1 + 5) / 2. Hint: Let ψ = (1 − 5) / 2. Use induction and the definition of
+;; the Fibonacci numbers (see 1.2.2) to prove that
+;; Fib(n) = (φ^n − ψ^n) / 5.
